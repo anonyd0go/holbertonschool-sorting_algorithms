50a95f50beb06c34965ea2fe3287de8349baa3fb
DDF <133790676+anonyd0go@users.noreply.github.com>
2024-12-11 18:14:56 -0400

    Add bubble sort algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,31 @@
+#include <stddef.h>
+#include "sort.h"
+
+
+/**
+ * bubble_sort - Sorts an array of ints in ascending order
+ * @array: Array of ints
+ * @size: size of the array
+ * Return: void
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t swap = 0, i;
+	int temp;
+
+	do {
+		swap = 0;
+		print_array(array, size);
+		for (i = 0; i < size - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swap = 1;
+			}
+		}
+	} while (swap != 0);
+}
+
