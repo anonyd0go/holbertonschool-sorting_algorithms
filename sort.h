0cdf2b99209f3fb4a238bb432b0de127b06b0921
Judith <j.espinalm12@gmail.com>
2024-12-13 14:34:38 -0400

    Correct comment for betty

--- sort.h
+++ sort.h
@@ -1,5 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
+
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -20,4 +21,5 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
 #endif
